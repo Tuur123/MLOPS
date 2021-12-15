4248149f0420f8dabdbd17bc7de8d2793136c6c2
Arthur d'Hooge <arthur.dhooge@outlook.com>
2021-12-15 16:42:09 +0100

    azure integration template

--- API/app/main.py
+++ API/app/main.py
@@ -1,11 +1,13 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+import requests
 
 app = FastAPI()
 
 class MovieList(BaseModel):
     movies: list
 
+modelEndpoint = 'https://www.w3schools.com/python/demopage.php'
 
 @app.get("/")
 def read_root():
@@ -14,4 +16,10 @@ def read_root():
 
 @app.post("/movies/")
 async def create_movieList(movielist: MovieList):
-    return movielist
+
+    # stuur request naar azure model endpoint
+    response = requests.post(url, data = movielist)
+
+    # ontvang movies om aan te bevelen
+
+    return response.text
